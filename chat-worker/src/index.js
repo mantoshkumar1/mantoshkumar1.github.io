ae12fdf0d9e5113d8f27cd9f1ca12ae578cc3b33
Mantosh <mantoshk234@gmail.com>
2026-07-12 21:50:40 -0400

    chore: invalidate pre-fix Ask Mantosh responses

--- chat-worker/src/index.js
+++ chat-worker/src/index.js
@@ -11,7 +11,7 @@ import { enforceRateLimit } from "./rate-limit.js";
 import { retrieveKnowledge } from "./retrieval.js";
 import { parseChatRequest } from "./validation.js";
 
-const ANSWER_POLICY_VERSION = "visitor-intent-v7";
+const ANSWER_POLICY_VERSION = "visitor-intent-v8";
 
 function json(body, status, origin, extraHeaders = {}) {
   const headers = corsHeaders(origin);
